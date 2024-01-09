94d6bca5af1ec168eef347dd437944070207e2af
Tomasz Suski <tomasz.suski@orba.co>
2024-01-09 14:56:32 +0100

    added actions dispatching via useDispatch hook

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,10 +1,19 @@
 import React from "react";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { CounterState } from "../store";
 import classes from "./Counter.module.css";
 
 const Counter = () => {
   const counter = useSelector((state: CounterState) => state.counter);
+  const dispatch = useDispatch();
+
+  const incrementHandler = () => {
+    dispatch({ type: "INCREMENT" });
+  };
+
+  const decrementHandler = () => {
+    dispatch({ type: "DECREMENT" });
+  };
 
   const toggleCounterHandler = () => {};
 
@@ -12,6 +21,10 @@ const Counter = () => {
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
       <div className={classes.value}>{counter}</div>
+      <div>
+        <button onClick={() => decrementHandler()}>Decrement</button>
+        <button onClick={() => incrementHandler()}>Increment</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
