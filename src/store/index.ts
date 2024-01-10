3673c5d7957e7fe6bb58503a14a7c2591fabec96
Tomasz Suski <tomasz.suski@orba.co>
2024-01-10 14:03:31 +0100

    added state slice via createSlice from redux-toolkit

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,4 +1,5 @@
 import { createStore } from "redux";
+import { createSlice } from "@reduxjs/toolkit";
 
 export interface CounterState {
   counter: number;
@@ -12,10 +13,41 @@ export type Actions =
   | { type: "INCREASE_BY_NUMBER"; payload: number }
   | { type: "DECREASE_BY_NUMBER"; payload: number };
 
+
+const initialState: CounterState = { counter: 0, showCounter: true };
+
+// createSlice automatycznie generuje akcje i reducery
+// nie trzeba pisać switcha
+// przy pracy z redux-toolkit nie da się przypadkowo zmodyfikować stanu
+// więc zapis jak poniżej zawsze zaowocuje nowym stanem
+// toolkit zadba o klonowanie stanu i nie będzie modyfikował go bezpośrednio
+createSlice({
+  name: "counter",
+  initialState,
+  reducers: {
+    increment(state) {
+      state.counter++;
+    },
+    decrement(state) {
+      state.counter--;
+    },
+    increaseByNumber(state, action: { payload: number }) {
+      state.counter += action.payload;
+    },
+    decreaseByNumber(state, action: { payload: number }) {
+      state.counter -= action.payload;
+    },
+    toggle(state) {
+      state.showCounter = !state.showCounter;
+    },
+  },
+});
+
 const counterReducer = (
-  state = { counter: 0, showCounter: true },
+  state = initialState,
   action: Actions
 ) => {
+ 
   switch (action.type) {
     case "INCREMENT":
       // przy pracy z obiektami czy tablicami, nie można modyfikować stanu, tylko zwracać nowy
